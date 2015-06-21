cdb8d3ec6c2d769ae0c63d8bb0875861db79a0b1
cmondies <cmondies@mac.com>
2015-06-21 23:46:36 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Starting up Tor briefly to generate your hidden service hostname. Please wait..."
-service tor start & sleep 10; service tor stop ; echo "Your hostname:" ; cat /var/lib/tor/hidden_service/hostname
 service tor start
+sleep 1
+tail -f /var/log/tor/log
